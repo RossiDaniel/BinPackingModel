e3aa05491557284a16e571bdc4e45a59892d1ba2
Unknown <bovo.96@gmail.com>
2018-10-02 11:22:23 +0200

    sfida

--- CyLP/model.py
+++ CyLP/model.py
@@ -1,6 +1,7 @@
 import numpy as np
-from cylp.py.modeling.CyLPModel import CyLPArray
 from cylp.cy import CyClpSimplex
+from cylp.py.modeling.CyLPModel import CyLPArray
+
 s = CyClpSimplex()
 
 # Add variables
@@ -15,3 +16,16 @@ a = CyLPArray([5, 2.5])
 b = CyLPArray([4.2, 3])
 x_u= CyLPArray([2., 3.5])
 
+# Add constraints
+s += A * x <= a
+s += 2 <= B * x + D * y <= b
+#s += y >= 0
+s += 1.1 <= x[1:3] <= x_u
+
+# Set the objective function
+c = CyLPArray([1., -2., 3.])
+s.objective = c * x + 2 * y.sum()
+
+# Solve using primal Simplex
+s.primal()
+print s.primalVariableSolution['x']
